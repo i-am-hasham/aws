1b47c2cb62c699b185ebfce46597d48a7743dcd0
i-am-hasham <hashambaig2000@gmail.com>
2026-04-27 15:25:12 +0500

    codedeploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull hasham17/python-project
+
+# Run the Docker image as a container
+docker run -d 5000:5000 hasham17/python-project
