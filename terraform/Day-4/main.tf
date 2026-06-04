a467ec3133200e0832ca70c8a0d474e0d9513932
i-am-hasham <baighasham80@gmail.com>
2026-06-04 12:11:00 +0500

    adding file

--- terraform/Day-4/main.tf
+++ terraform/Day-4/main.tf
@@ -2,17 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "hasham" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  ami = "ami-091138d0f0d41ff90" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "hasham-s3-demo-xyz" # change this
+}
+resource "aws_s3_bucket" "s3_bucket_2" {
+  bucket = "hasham12-s3-demo-xyz" # change this
 }
 
-resource "aws_dynamodb_table" "terraform_lock" {
+/*resource "aws_dynamodb_table" "terraform_lock" {
   name           = "terraform-lock"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
@@ -22,3 +24,4 @@ resource "aws_dynamodb_table" "terraform_lock" {
     type = "S"
   }
 }
+*/
