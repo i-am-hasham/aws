ba4081c9db9e0fa5c333ce00c2c5b3d1b6102188
i-am-hasham <baighasham80@gmail.com>
2026-06-15 15:55:57 +0500

    adding file

--- terraform/Day-7/main.tf
+++ terraform/Day-7/main.tf
@@ -3,30 +3,30 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address = "http://54.175.23.71:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id = "c20fd306-669d-f095-eebc-1467b5e8eda9"
+      secret_id = "70e8136e-9105-7c6d-8c81-a56edc04b82b"
     }
   }
 }
 
 data "vault_kv_secret_v2" "example" {
-  mount = "secret" // change it according to your mount
+  mount = "secrets" // change it according to your mount
   name  = "test-secret" // change it according to your secret
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = "ami-091138d0f0d41ff90"
   instance_type = "t2.micro"
 
   tags = {
     Name = "test"
-    Secret = data.vault_kv_secret_v2.example.data["foo"]
+    Secret = data.vault_kv_secret_v2.example.data["username"]
   }
 }
